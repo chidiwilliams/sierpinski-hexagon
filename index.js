69ce7bd5c6b528a9d7189c6434b1e9adf6e15093
Chidi Williams <williamschidi1@gmail.com>
2020-06-18 21:16:34 +0100

    Updates

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54331" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59538" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
